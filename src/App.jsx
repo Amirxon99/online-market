1b372f227b1f7cebf9641124c5159ffceff59203
Mirabbos Sobitjonov <sobitjonovmirabbos1@gmail.com>
2025-05-30 19:33:02 +0500

    afasdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css"
 import Header from "./components/header/Header"
-
 function App() {
   return (
     <div>
---
+++ src/components/section3/Section3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Section3() {
+  return (
+    <div>
+        <h1>Musayev Abduvahob 2009-yil</h1>
+    </div>
+  )
+}
+
+export default Section3
