a31da6c2f319c625b78b29bfa966362a09fa5448
hadiyatulloh <hadiyatullohfayzullayev@gmail.com>
2025-05-30 19:27:00 +0500

    section1

---
+++ src/components/section1/Section1.jsx
@@ -0,0 +1,11 @@
+import styles from './section1.module.css'
+
+function Section1() {
+  return (
+    <div className={styles.section1}>
+        <h1></h1>
+    </div>
+  )
+}
+
+export default Section1
